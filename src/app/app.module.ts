8046f9d6ed36e757b0132d9c3d3adcd49d73ea47
mcbryan1 <git config --global user.email solomonmcbryan@gmail.comgit config --global user.email>
2020-10-18 03:07:00 +0000

    done With Services/Features Section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {LandingPageComponent} from './components/landing-page/landing-page.component'
 import { ApiComponent } from './components/api/api.component';
 import { PricingComponent } from './components/pricing/pricing.component';
+import { ServicesComponent } from './components/services/services.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
     ApiComponent,
-    PricingComponent
+    PricingComponent,
+    ServicesComponent,
+    FooterComponent
   ],
   imports: [
   BrowserModule,
